0683d935e0471a003824273af28a555f6d0bcf38
Rushikesh Prasad Karkhanis <karkhanisrushi@Rushikeshs-MacBook-Pro.local>
2026-01-11 15:48:33 +0530

    Initial Commit for User

---
+++ user/src/main/java/com/tk/user/resource/UserController.java
@@ -0,0 +1,15 @@
+package com.tk.user.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+    @GetMapping("/users")
+    public String getUsers() {
+        return "User";
+    }
+}
